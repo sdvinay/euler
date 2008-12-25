1c584b860c004c551d438335ea3dd6280747618e
Vinay Kumar <sdvinay@gmail.com>
2008-12-25 04:13:24 -0800

    Made many of the import lines more specific in 16.hs

--- 16.hs
+++ 16.hs
@@ -1,7 +1,7 @@
 -- works!
 -- sum of the digits of 2^1000
 
-import Data.Char -- for digitToInt
+import Data.Char (digitToInt)
 
 answer = sumDigits (2^1000)
 	where sumDigits num = sum(map digitToInt (show num))
