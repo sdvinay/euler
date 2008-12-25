0119eb7ae34e60502d37d16747fc3dd8e20ab992
Vinay Kumar <sdvinay@gmail.com>
2008-12-25 00:45:31 -0800

    Cleanup of 18.hs and BinaryTreeOfInts.hs Came up with a cleaner makeTree function to generate a tree from the input strings.  Moved this functionality into the BinaryTreeOfInts module.

--- BinaryTreeOfInts.hs
+++ BinaryTreeOfInts.hs
@@ -10,3 +10,21 @@ longestPath :: BTreeInt -> Int
 longestPath Empty = 0
 longestPath node = (val node) + maximum (map longestPath [left node, right node])
 
+makeTree :: [String] -> BTreeInt
+makeTree input = head $ foldr makeNodes [] input
+	where
+	makeLeaves       :: String -> [BTreeInt]
+	makeLeaves row = map makeLeafNode $ words row
+		where 
+		makeLeafNode :: String -> BTreeInt
+		makeLeafNode str = (Node ((read str)::Int) Empty Empty)
+	makeNodes       :: String -> [BTreeInt] -> [BTreeInt]
+	makeNodes row [] = makeLeaves row
+	makeNodes row children = zipWith makeNode (words row) pairChildren
+		where
+		pairChildren :: [(BTreeInt, BTreeInt)]
+		pairChildren = zip children (tail children)
+		makeNode  :: String -> (BTreeInt, BTreeInt) -> BTreeInt
+		makeNode str chldrn = Node ((read str)::Int) (fst chldrn) (snd chldrn)
+
+	
