c8209e97dea51bc82f8c55994f7f1ab903699e62
Vinay Kumar <sdvinay@gmail.com>
2008-12-24 21:10:38 -0800

    Solved 28 in Haskell

---
+++ 28.hs
@@ -0,0 +1,13 @@
+-- works!
+-- What is the sum of both diagonals in a 1001 by 1001 spiral formed in the same way?
+
+-- note that in an NxN spiral, the top-right diagonal is the sum of the first
+-- N odd squares, and the bottom-left are the numbers exactly halfway between them
+-- and the other two diagonals are the same as the bottom-left
+
+size=5
+
+squares size = map (\n->n*n) [1,3..size]
+averages squares = zipWith (\a b->div(a+b) 2) squares (tail squares)
+
+diagsum size = sum (squares size) + 3* sum(averages (squares size))
