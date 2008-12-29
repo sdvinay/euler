3c1e2ea9af5cc518e0bff23a930a331f60f0c864
Vinay Kumar <sdvinay@gmail.com>
2008-12-28 22:27:37 -0800

    solved 37 in Haskell (but it's reeealllly slow)

---
+++ 37.hs
@@ -0,0 +1,20 @@
+-- works (but is really dang slow)
+-- Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+import Primes
+import Data.List
+
+isTruncatablePrime prm = all isPrime $ map (\str -> read str::Int) $ truncations $ show prm
+
+truncations str = union (truncationsL str) (truncationsR str)
+
+truncationsL (char:chars) = (char:chars) : truncationsL chars
+truncationsL [] = []
+
+truncationsR str = map reverse (truncationsL $ reverse str)
+
+
+answer = sum $ take 11 $ filter isTruncatablePrime candidates
+	where candidates = dropWhile (<= 7) primes
+	
+
