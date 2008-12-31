5ce02e4866b03064e48136fa634c64436dd9e0a5
Vinay Kumar <sdvinay@gmail.com>
2008-12-31 02:19:49 -0800

    Divisors.hs: made the divisors function more efficient Specifically, only look for divisors up to the square root of n (the "small divisors"), then compute the large divisors by dividing the smalls into n.  The new implementation is O(sqrt(n)+divisors(n)), whereas the old version was O(n).

--- Divisors.hs
+++ Divisors.hs
@@ -1,6 +1,18 @@
 module Divisors (divisors) where
 
+import Data.List
+
+-- find divisors by first finding "small divisors", those less than sqrt(n)
+-- then find the "large divisors", which are the corresponding divisors of the smalls
 divisors :: Int -> [Int]
-divisors n = filter (\x -> divides n x) [1 .. (div n 2)]
+divisors n = sort $ union smalls bigs
+	where
+	smalls = small_divisors n
+	bigs = large_divisors n smalls
+
+small_divisors n = filter (divides n) $ takeWhile (\x -> x*x <= n) [1..]
 	where divides x y = (mod x y) == 0
 
+-- we don't want to count a number as its own divisor, so filter 1 out of the smalls
+-- before computing the large divisors
+large_divisors n smalls = map (\x -> div n x) $ filter (/= 1) smalls
