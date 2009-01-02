ced1df53df87500b30edafefb8a4ba6b27ae23f7
Vinay Kumar <sdvinay@gmail.com>
2009-01-01 19:57:23 -0800

    Some cleanup to 14.hs (still not scaling enough)

--- 14.hs
+++ 14.hs
@@ -3,24 +3,27 @@
 -- http://projecteuler.net/index.php?section=problems&id=14
 
 seqlen :: (Integral a, Integral b) => a -> b
-seqlen n | n==1 = 1
-         | otherwise = 1+seqlen(nextInSeq n)
+seqlen 1 = 1
+seqlen n = 1+seqlen(nextInSeq n)
 
 
-nextInSeq n | mod n 2 == 1 = 3*n+1
-			| otherwise    = div n 2
+nextInSeq n | odd n     = 3*n+1
+            | otherwise = div n 2
 
 type MyPair = (Integer, Integer)
 
 makePairs :: Integer -> Integer -> [MyPair]
 makePairs start end = zip [start .. end] (map seqlen [start .. end])
 
-lt :: MyPair -> MyPair -> Bool
-lt p1 p2 = snd(p1) < snd(p2)
-
 maxPr :: MyPair -> MyPair -> MyPair
-maxPr p1 p2 | lt p1  p2 = p2
-            | otherwise = p1
+maxPr p1 p2 | snd(p1) < snd(p2) = p2
+            | otherwise         = p1
 
 longestPath n = foldr1 (maxPr) $ makePairs 1 n
+{-
+longestPath n = maximum $ map seqlen [1..n]
+answer n = filter (\x-> seqlen x == longest) [1..n]
+	where longest = longestPath n
+-}
+
 
