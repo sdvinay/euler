91f4bf7c8b516e123cb22bc85a57a1e5fb2124ec
Vinay Kumar <sdvinay@gmail.com>
2015-08-09 00:54:56 -0700

    72.hs in progress: start computing rather than enumerating

--- 72.hs
+++ 72.hs
@@ -1,6 +1,18 @@
+import Primes
+
+-- Count the unique fractions up to a certain denominator limit
+--  (limit=10^6 in this case)
+-- Enumerating the fractions works, but is way too slow for
+--  that limit.  So we need to look at calculating.
 
 fractionsUpTo limit = concatMap fractions [1..limit]
---[(n,d)| d<-[1..limit],n<-[1..d], n<d, (gcd n d) ==1] 
 
+-- Enumerate the fractions for a denominator by checking the gcd
+--  of each numerator.
 fractions d = [(n,d)| n<-[1..d], n<d, (gcd n d) ==1] 
 
+-- Compute the number of fractions for a denominator by 
+--  subtracting multiples of prime factors
+numFractions :: Int-> Int
+numFractions d | isPrime d = d-1
+               | otherwise = 0 -- TODO this is placeholding, not correct
