5ebbafab6c51f3233c3c3d2ed85475fc5909c3de
Vinay Kumar <sdvinay@gmail.com>
2015-08-09 16:26:29 -0700

    Sped up isPrime by 40% by skipping even divisors

--- primes.hs
+++ primes.hs
@@ -3,9 +3,9 @@ module Primes
       isPrime,
       primes) where
 
--- current implmenentation: take 30000 primes takes ~34 sec
--- current implmenentation: take 20000 primes takes ~20 sec
--- current implmenentation: take 10000 primes takes 7-8 sec
+-- current implmenentation: take 30000 primes takes ~19 sec
+-- current implmenentation: take 20000 primes takes ~11 sec
+-- current implmenentation: take 10000 primes takes 4-5 sec
 -- timings are on MacBook Air, of CPU time 
 
 primeFactors :: Integer -> [Integer]
@@ -16,12 +16,15 @@ primeFactors n = pfs n 2
       | mod n start == 0 = start:(pfs ( div n start) start)
       | otherwise = pfs n (start+1)
 
-isPrime n = isP n 2
+isPrime 4 = False
+isPrime 6 = False
+isPrime 8 = False
+isPrime n = isP n 3
   where
     isP n start
       | n < (start * start) = True
       | mod n start == 0 = False
-      | otherwise = isP n (start+1)
+      | otherwise = isP n (start+2)
 
 primes = 2 : filter isPrime [3,5..]
 
