a308401997ddf2c48c30846f692a98cdc859b814
Vinay Kumar <sdvinay@gmail.com>
2015-08-09 00:04:12 -0700

    72.hs in progress:  get fractions by denominator

--- 72.hs
+++ 72.hs
@@ -1,2 +1,6 @@
 
-fractions limit = [(n,d)| d<-[1..limit],n<-[1..d], n<d, (gcd n d) ==1] 
+fractionsUpTo limit = concatMap fractions [1..limit]
+--[(n,d)| d<-[1..limit],n<-[1..d], n<d, (gcd n d) ==1] 
+
+fractions d = [(n,d)| n<-[1..d], n<d, (gcd n d) ==1] 
+
