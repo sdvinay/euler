8be80b080628284c53f41508d60028fd6d554b39
Vinay Kumar <sdvinay@gmail.com>
2015-08-09 12:48:36 -0700

    Extended Primes.hs to do primeFactors, for prob72

--- primes.hs
+++ primes.hs
@@ -10,6 +10,14 @@ largestPrimeFactor n = lpf n 2
       | mod n start == 0 = lpf ( div n start) start
       | otherwise = lpf n (start+1)
 
+primeFactors :: Integer -> [Integer]
+primeFactors n = pfs n 2
+  where
+    pfs n start
+      | n < (start * start) = [n]
+      | mod n start == 0 = start:(pfs ( div n start) start)
+      | otherwise = pfs n (start+1)
+
 isPrime x | x > 1 =  (largestPrimeFactor x) == x
           | otherwise = False
 
