410aef0d9f8c779f75f86a3284c49dc5c590baae
Vinay Kumar <sdvinay@gmail.com>
2015-08-10 21:18:28 -0700

    Remove dependency on System/argv from 35.hs

--- 35.hs
+++ 35.hs
@@ -1,9 +1,7 @@
 -- works!
 -- How many circular primes are there below one million
--- make using 'ghc Primes.hs 35.hs -o 35.out'
 
 import Primes (isPrime, primes)
-import System (getArgs)
 
 rotations :: String-> [String]
 rotations str = map (\n->rotate str n) [0..(length str - 1)]
@@ -18,5 +16,3 @@ isCircularPrime prm = all isPrime (rotatedInts prm)
 
 answer n = length (filter isCircularPrime (takeWhile (<n) primes))
 
-main = do args <- getArgs;
-		print (answer(read(args!!0)::Integer))
