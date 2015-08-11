32becdbd7cc74e63843d4f67c8917b88ebaa06fb
Vinay Kumar <sdvinay@gmail.com>
2015-08-10 22:37:26 -0700

    49.hs fix compiler warnings

--- 49.hs
+++ 49.hs
@@ -5,7 +5,7 @@ import Data.List (sort) -- used to identify permutations
 import Primes
 
 data Sequence = Seq {lowest, step :: Int}
-	deriving Show
+    deriving Show
 
 l :: Sequence -> Int
 l sequence = lowest sequence
@@ -15,16 +15,16 @@ h sequence = lowest sequence + 2 * step sequence
 isPrimeSequence sequence = all isPrime [l sequence,m sequence,h sequence]
 
 isPermutedSequence seq = (isPermutation (l seq) (m seq)) &&
-						 (isPermutation (l seq) (h seq))
-	where isPermutation num1 num2 = sort (show num1) == sort (show num2)
+                         (isPermutation (l seq) (h seq))
+    where isPermutation num1 num2 = sort (show num1) == sort (show num2)
 
 
 getSequences lowest = filter isPermutedSequence $ filter isPrimeSequence seqs
-	where 
-	seqs = map (\step -> Seq lowest step) [1..(maxStep lowest)]
-		where maxStep lowest = div (9999-lowest) 2
+    where 
+    seqs = map (\step -> Seq lowest step) [1..(maxStep lowest)]
+        where maxStep lowest = div (9999-lowest) 2
 
 answer = map makenumber $ concatMap getSequences candidates
-	where 
-	candidates = takeWhile(<10000) $ dropWhile(<1000) primes
-	makenumber seq = (l seq)*10000*10000 + (m seq)*10000 + (h seq)
+    where 
+    candidates = takeWhile(<10000) $ dropWhile(<1000) primes
+    makenumber seq = (l seq)*10000*10000 + (m seq)*10000 + (h seq)
