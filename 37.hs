c5695480a620545761ee4fd524d1b406d1cb7d5a
Vinay Kumar <sdvinay@gmail.com>
2015-08-10 22:34:02 -0700

    Added verification check to 37.hs

--- 37.hs
+++ 37.hs
@@ -1,5 +1,6 @@
 -- works (but is really dang slow)
 -- Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+-- https://projecteuler.net/problem=37
 
 import Primes
 import Data.List
@@ -17,4 +18,4 @@ truncationsR str = map reverse (truncationsL $ reverse str)
 answer = sum $ take 11 $ filter isTruncatablePrime candidates
     where candidates = dropWhile (<= 7) primes
     
-
+check = answer==748317
