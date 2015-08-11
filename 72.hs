582609cf5a48b25199a1d4c5a25288de74f4835a
Vinay Kumar <sdvinay@gmail.com>
2015-08-10 21:46:59 -0700

    Progress on 72.hs

--- 72.hs
+++ 72.hs
@@ -1,4 +1,5 @@
 import Primes
+import Data.List
 
 -- Count the unique fractions up to a certain denominator limit
 --  (limit=10^6 in this case)
@@ -14,12 +15,21 @@ fractions d = [(n,d)| n<-[1..d], n<d, (gcd n d) ==1]
 -- Compute the number of fractions for a denominator by 
 --  subtracting multiples of prime factors
 numFractions :: Int-> Int
-numFractions d | isPrime d = d-1
-               | otherwise = 0 -- TODO this is placeholding, not correct
+numFractions d 
+ | isPrime d = d-1
+ | otherwise = d - 1 - unreducedNumerators
+   where
+     -- this pattern is correct, but needs to be repeated beyond double-counts; if there are >2 pfs, then we have triplecounts, etc TODO
+     unreducedNumerators = multiples - doublecounts
+       where
+         pfs = nub ( primeFactors d)
+         pfProducts = [x*y|x<-pfs, y<-pfs, x<y]
+         multiples = sum $ map (\x-> div (d-1) x) pfs
+         doublecounts = sum $ map (\x-> div (d-1) x) pfProducts
 
 -- creates a tuple of the two different computations from the one denominator
 compareComputations d = (length $ fractions d, numFractions d)
 
 -- if the two values in the tuple match, that means the computations give the same result
 -- if a 2-tuple has varying values, that means the calcs gave different values!
-test = map compareComputations [72,67,1, 2, 100, 53, 13]
+test = map compareComputations [72,67,1, 2, 100, 53, 13, 60, 14, 30, 2*2*3*5*7*11]
